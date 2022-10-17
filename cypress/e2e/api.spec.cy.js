57a7854ae0c1bdec379d305452b3d7b911256fed
Pardeep <pardeep.rj90@gmail.com>
2022-10-17 17:54:34 +0530

    Change Committed

--- cypress/e2e/api.spec.cy.js
+++ cypress/e2e/api.spec.cy.js
@@ -1,18 +1,15 @@
 /// <reference types="cypress"/>
 
 describe('Rest API Testing using Cypress', () => {
-  it('API Test --Header Validations', () => {
-    cy.request('https://pokeapi.co/api/v2/pokemon/25/').as('APIResponse')
-    cy.get('@APIResponse').its('headers').its('content-type').should('include','application/json')
-  })
-
-  it('API Test --Status Code Validation', ()=>{
-    cy.request('https://pokeapi.co/api/v2/pokemon/25/').as('APIResponse')
-    cy.get('@APIResponse').its('status').should('eq',200)
-  })
-
-  it('Validate Name Value',()=>{
-    cy.request('https://pokeapi.co/api/v2/pokemon/25/').as('APIResponse')
-    cy.get('@APIResponse').its('body').should('include',{name:'Pardeep'})
+  it('Get Users',()=>{
+    cy.request({
+      methos:'GET',
+      url:'https://gorest.co.in/public/v2/users',
+      Headers:{
+        'authorization':'Bearer 779dd21efd508709c535821dd39dec403e128c2901263bce7da2aa701541aa89	'
+      }
+    }).then((res)=>{
+      expect(res.status).to.eq(200)
+    })
   })
 })
