5fdbd9efab1a4cbacd33f15f7b0c434df6dc55fc
Pardeep <pardeep.rj90@gmail.com>
2022-10-17 15:53:34 +0530

    APITTesting

---
+++ cypress/e2e/api.spec.cy.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress"/>
+
+describe('Rest API Testing using Cypress', () => {
+  it('API Test --Header Validations', () => {
+    cy.request('https://pokeapi.co/api/v2/pokemon/25/').as('APIResponse')
+    cy.get('@APIResponse').its('headers').its('content-type').should('include','application/json')
+  })
+
+  it('API Test --Status Code Validation', ()=>{
+    cy.request('https://pokeapi.co/api/v2/pokemon/25/').as('APIResponse')
+    cy.get('@APIResponse').its('status').should('eq',200)
+  })
+})
